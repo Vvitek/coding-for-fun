dfcd737b3ae7e015a91c66342f9c02cf4172c8b8
Witold Rudnicki <witold.m.rudnicki@gsk.com>
2022-12-04 00:05:10 +0100

    get_n_max_calories and sum_n_max_calories

--- t_01_01/t_01_01.py
+++ t_01_01/t_01_01.py
@@ -4,15 +4,26 @@ input_data_path = Path(__file__).with_name("data.txt")
 
 
 def get_sum_calories(inventory: str) -> int:
-    return sum([int(calories_amount) for calories_amount in inventory.split()])
+    return sum((int(calories_amount) for calories_amount in inventory.split()))
 
 
 def get_max_calories(input_data: str) -> int:
     return max(
-        [get_sum_calories(inventory) for inventory in input_data.split(sep="\n\n")]
+        (get_sum_calories(inventory) for inventory in input_data.split(sep="\n\n"))
     )
+
+
+def get_n_max_calories(input_data: str, n_most_calories: int) -> tuple[int]:
+    return sorted(
+        (get_sum_calories(inventory) for inventory in input_data.split(sep="\n\n")),
+        reverse=True,
+    )[:n_most_calories]
+
+
+def sum_n_max_calories(input_data: str, n_most_calories: int) -> int:
+    return sum(get_n_max_calories(input_data, n_most_calories))
 
 
 if __name__ == "__main__":
     with input_data_path.open("r") as input_file:
-        print(get_max_calories(input_file.read()))
+        print(sum_n_max_calories(input_file.read(), 3))
