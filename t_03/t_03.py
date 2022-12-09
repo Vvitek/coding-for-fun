a60c564b3fcc6c65380f0203bbd28b7c416f9a96
Witold Rudnicki <witold.m.rudnicki@gsk.com>
2022-12-09 11:46:31 +0100

    task 3 part 1 implementation

--- t_03/t_03.py
+++ t_03/t_03.py
@@ -1,2 +1,29 @@
+import string
+from pathlib import Path
+
+input_data_path = Path(__file__).with_name("data.txt")
+
+
 def get_same_element_in_both_compartments(elements: str) -> str:
-    return elements[0]
+    second_compartment = set(elements[len(elements) // 2 :])
+    for i in elements:
+        if i in second_compartment:
+            return i
+
+
+def get_priority(element: str) -> int:
+    return string.ascii_letters.index(element) + 1
+
+
+def get_priority_sum(rucksacks: list[str]) -> int:
+    return sum(
+        [
+            get_priority(get_same_element_in_both_compartments(rucksack))
+            for rucksack in rucksacks
+        ]
+    )
+
+
+if __name__ == "__main__":
+    with input_data_path.open("r") as input_file:
+        print(get_priority_sum(input_file.readlines()))
--- t_03/test_t03.py
+++ t_03/test_t03.py
@@ -1,5 +1,5 @@
 import pytest
-from t_03 import get_same_element_in_both_compartments
+from t_03 import get_same_element_in_both_compartments, get_priority, get_priority_sum
 
 
 @pytest.mark.parametrize(
@@ -15,3 +15,31 @@ from t_03 import get_same_element_in_both_compartments
 )
 def test_get_same_element_in_both_compartments(elements, expected_result):
     assert get_same_element_in_both_compartments(elements) == expected_result
+
+
+@pytest.mark.parametrize(
+    "element, expected_result",
+    [("p", 16), ("L", 38), ("P", 42), ("v", 22), ("t", 20), ("s", 19)],
+)
+def test_get_priority(element, expected_result):
+    assert get_priority(element) == expected_result
+
+
+@pytest.mark.parametrize(
+    "elements, expected_result",
+    [
+        (
+            [
+                "vJrwpWtwJgWrhcsFMMfFFhFp",
+                "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL",
+                "PmmdzqPrVvPwwTWBwg",
+                "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn",
+                "ttgJtRGJQctTZtZT",
+                "CrZsJsPPZsGzwwsLwLmpwMDw",
+            ],
+            157,
+        )
+    ],
+)
+def test_get_priority_sum(elements, expected_result):
+    assert get_priority_sum(elements) == expected_result
