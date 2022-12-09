267da0ea14756bedccb36e7d88564cdf18785911
Witold Rudnicki <witold.m.rudnicki@gsk.com>
2022-12-09 23:49:43 +0100

    task 3 implementation

--- t_03/t_03.py
+++ t_03/t_03.py
@@ -1,5 +1,6 @@
 import string
 from pathlib import Path
+from typing import Generator, TextIO
 
 input_data_path = Path(__file__).with_name("data.txt")
 
@@ -24,6 +25,27 @@ def get_priority_sum(rucksacks: list[str]) -> int:
     )
 
 
+def get_common_item(rucksacks: tuple[str]) -> str:
+    return next(
+        j for j in (i for i in rucksacks[0] if i in rucksacks[1]) if j in rucksacks[2]
+    )
+
+
+def split_into_3_lines(
+    input_file: TextIO,
+) -> Generator[tuple[tuple[str]], None, None]:
+    return (
+        (line1.rstrip(), line2.rstrip(), line3.rstrip())
+        for line1, line2, line3 in zip(input_file, input_file, input_file)
+    )
+
+
+def get_common_badge_priorities(input_file: TextIO) -> Generator[int, None, None]:
+    return (
+        get_priority(get_common_item(line)) for line in split_into_3_lines(input_file)
+    )
+
+
 if __name__ == "__main__":
     with input_data_path.open("r") as input_file:
-        print(get_priority_sum(input_file.readlines()))
+        print(sum(get_common_badge_priorities(input_file)))
--- t_03/test_t03.py
+++ t_03/test_t03.py
@@ -1,5 +1,14 @@
 import pytest
-from t_03 import get_same_element_in_both_compartments, get_priority, get_priority_sum
+from pathlib import Path
+
+from t_03 import (
+    get_same_element_in_both_compartments,
+    get_priority,
+    get_priority_sum,
+    get_common_item,
+    split_into_3_lines,
+    get_common_badge_priorities,
+)
 
 
 @pytest.mark.parametrize(
@@ -43,3 +52,65 @@ def test_get_priority(element, expected_result):
 )
 def test_get_priority_sum(elements, expected_result):
     assert get_priority_sum(elements) == expected_result
+
+
+@pytest.mark.parametrize(
+    "rucksacks, expected_result",
+    [
+        (
+            (
+                "vJrwpWtwJgWrhcsFMMfFFhFp",
+                "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL",
+                "PmmdzqPrVvPwwTWBwg",
+            ),
+            "r",
+        ),
+        (
+            (
+                "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn",
+                "ttgJtRGJQctTZtZT",
+                "CrZsJsPPZsGzwwsLwLmpwMDw",
+            ),
+            "Z",
+        ),
+    ],
+)
+def test_get_common_item(rucksacks, expected_result):
+    assert get_common_item(rucksacks) == expected_result
+
+
+@pytest.mark.parametrize(
+    "split_line_file_path, expected_result",
+    [
+        (
+            "split_lines_data.csv",
+            (
+                (
+                    "vJrwpWtwJgWrhcsFMMfFFhFp",
+                    "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL",
+                    "PmmdzqPrVvPwwTWBwg",
+                ),
+                (
+                    "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn",
+                    "ttgJtRGJQctTZtZT",
+                    "CrZsJsPPZsGzwwsLwLmpwMDw",
+                ),
+            ),
+        )
+    ],
+)
+def test_split_into_3_lines(split_line_file_path, expected_result):
+    with Path(__file__).with_name(split_line_file_path).open("r") as input_file:
+        line_generator = split_into_3_lines(input_file)
+        assert next(line_generator) == expected_result[0]
+        assert next(line_generator) == expected_result[1]
+
+
+@pytest.mark.parametrize(
+    "split_line_file_path, expected_result", [("split_lines_data.csv", [18, 52])]
+)
+def test_get_common_badge_priorities(split_line_file_path, expected_result):
+    with Path(__file__).with_name(split_line_file_path).open("r") as input_file:
+        common_badge_priority_generator = get_common_badge_priorities(input_file)
+        assert next(common_badge_priority_generator) == expected_result[0]
+        assert next(common_badge_priority_generator) == expected_result[1]
