7f6dea2ef2cc52650311ddc2b0f942ad0520b9bf
Witold Rudnicki <witold.m.rudnicki@gsk.com>
2022-12-10 23:38:52 +0100

    task 4 second part

--- t_04/t_04.py
+++ t_04/t_04.py
@@ -4,7 +4,7 @@ input_data_path = Path(__file__).with_name("data.txt")
 
 
 def prepare_ranges(line):
-    return [[int(j) for j in i.split("-")] for i in line.split(",")]
+    return [[int(j) for j in i.split("-")] for i in line.strip().split(",")]
 
 
 def is_fully_contained(ranges):
@@ -17,10 +17,24 @@ def is_fully_contained(ranges):
     return False
 
 
+def is_partialy_contained(ranges):
+    for i in range(2):
+        if (
+            ranges[i][0] <= ranges[(i + 1) % 2][0] <= ranges[i][1]
+            or ranges[i][0] <= ranges[(i + 1) % 2][1] <= ranges[i][1]
+        ):
+            return True
+    return False
+
+
+def get_sum(input_file, method_to_run):
+    return sum((method_to_run(prepare_ranges(line)) for line in input_file))
+
+
 def get_nuber_of_fully_contained(input_file):
     return sum((is_fully_contained(prepare_ranges(line)) for line in input_file))
 
 
 if __name__ == "__main__":
     with input_data_path.open("r") as input_file:
-        print(get_nuber_of_fully_contained(input_file))
+        print(get_sum(input_file, is_partialy_contained))
--- t_04/test_t04.py
+++ t_04/test_t04.py
@@ -2,7 +2,12 @@ import pytest
 from pathlib import Path
 
 
-from t_04 import prepare_ranges, is_fully_contained, get_nuber_of_fully_contained
+from t_04 import (
+    prepare_ranges,
+    is_fully_contained,
+    is_partialy_contained,
+    get_sum,
+)
 
 
 @pytest.mark.parametrize(
@@ -39,7 +44,32 @@ def test_is_fully_contained(ranges, expected_result):
     assert is_fully_contained(ranges) == expected_result
 
 
+@pytest.mark.parametrize(
+    "ranges, expected_result",
+    [
+        ([[2, 3], [4, 5]], False),
+        ([[2, 4], [6, 8]], False),
+        ([[5, 7], [7, 9]], True),
+        ([[2, 8], [3, 7]], True),
+        ([[3, 7], [2, 8]], True),
+        ([[6, 6], [4, 6]], True),
+        ([[4, 6], [6, 6]], True),
+        ([[2, 6], [4, 8]], True),
+        ([[2, 6], [4, 8]], True),
+        ([[14, 59], [14, 59]], True),
+    ],
+)
+def test_is_partialy_contained(ranges, expected_result):
+    assert is_partialy_contained(ranges) == expected_result
+
+
 @pytest.mark.parametrize("input_file_path, expected_result", [("data_for_test.csv", 2)])
 def test_get_nuber_of_fully_contained(input_file_path, expected_result):
     with Path(__file__).with_name(input_file_path).open("r") as input_file:
-        assert get_nuber_of_fully_contained(input_file) == expected_result
+        assert get_sum(input_file, is_fully_contained) == expected_result
+
+
+@pytest.mark.parametrize("input_file_path, expected_result", [("data_for_test.csv", 4)])
+def test_get_nuber_of_partialy_contained(input_file_path, expected_result):
+    with Path(__file__).with_name(input_file_path).open("r") as input_file:
+        assert get_sum(input_file, is_partialy_contained) == expected_result
