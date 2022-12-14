71fa27c094c7d6241272fbd61e5fa54936d52706
Witold Rudnicki <witold.m.rudnicki@gsk.com>
2022-12-15 00:26:58 +0100

    task 6 part 1

--- t_06/t_06.py
+++ t_06/t_06.py
@@ -1,2 +1,16 @@
+from pathlib import Path
+
+input_data_path = Path(__file__).with_name("data.txt")
+
+
 def get_marker_position(input):
-    pass
+    four_signals = [i for i in input[:3]] + [None]
+    for i in range(3, len(input), 1):
+        four_signals[i % 4] = input[i]
+        if len(four_signals) == len(set(four_signals)):
+            return i + 1
+
+
+if __name__ == "__main__":
+    with input_data_path.open("r") as input_file:
+        print(get_marker_position(input_file.read()))
