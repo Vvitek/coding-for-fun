032f24fc9e7547c24a6a9952403842bcbd89cc0f
Witold Rudnicki <witold.m.rudnicki@gsk.com>
2022-12-15 00:40:10 +0100

    task 6 part 2

--- t_06/t_06.py
+++ t_06/t_06.py
@@ -3,14 +3,14 @@ from pathlib import Path
 input_data_path = Path(__file__).with_name("data.txt")
 
 
-def get_marker_position(input):
-    four_signals = [i for i in input[:3]] + [None]
-    for i in range(3, len(input), 1):
-        four_signals[i % 4] = input[i]
+def get_marker_position(input: str, singnal_length: int) -> int:
+    four_signals = [i for i in input[: singnal_length - 1]] + [None]
+    for i in range(singnal_length - 1, len(input), 1):
+        four_signals[i % singnal_length] = input[i]
         if len(four_signals) == len(set(four_signals)):
             return i + 1
 
 
 if __name__ == "__main__":
     with input_data_path.open("r") as input_file:
-        print(get_marker_position(input_file.read()))
+        print(get_marker_position(input_file.read(), 14))
--- t_06/test_t06.py
+++ t_06/test_t06.py
@@ -4,14 +4,15 @@ from t_06 import get_marker_position
 
 
 @pytest.mark.parametrize(
-    "input_string, expected_result",
+    "input_string, signal_length_expected_result_dict",
     [
-        ("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 7),
-        ("bvwbjplbgvbhsrlpgdmjqwftvncz", 5),
-        ("nppdvjthqldpwncqszvftbrmjlhg", 6),
-        ("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 10),
-        ("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 11),
+        ("mjqjpqmgbljsphdztnvjfqwrcgsmlb", ((4, 7), (14, 19))),
+        ("bvwbjplbgvbhsrlpgdmjqwftvncz", ((4, 5), (14, 23))),
+        ("nppdvjthqldpwncqszvftbrmjlhg", ((4, 6), (14, 23))),
+        ("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", ((4, 10), (14, 29))),
+        ("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", ((4, 11), (14, 26))),
     ],
 )
-def test_get_marker_position(input_string, expected_result):
-    assert get_marker_position(input_string) == expected_result
+def test_get_marker_position(input_string, signal_length_expected_result_dict):
+    for singnal_length, expected_result in signal_length_expected_result_dict:
+        assert get_marker_position(input_string, singnal_length) == expected_result
